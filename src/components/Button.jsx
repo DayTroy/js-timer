5607df7d34cb20ae396d6fbca8913fd0c26f0a2a
DayTroy <shadowyguy5@gmail.com>
2023-07-19 02:39:47 +0300

    Implemented the session and break lengths change with useReducer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,18 @@
-import { useEffect, useRef } from "react"
+import { useRef } from "react";
 
-const Button = ({value}) => {
-
-  // useEffect(() => {
-  //   handleChange()
-  // }, [])
+const Button = ({ value, onClick }) => {
   const ref = useRef(null);
-
-
-
-  const handleChange = () => {
-    ref.current.style.boxShadow = "2px 2px 0 0 #000"
+  const handleClick = () => {
+    ref.current.style.boxShadow = "2px 2px 0 0 #000";
     setTimeout(() => {
-      ref.current.style.boxShadow = "4px 4px 0 0 #000"
-    }, 100)
-  }
-    return (
-      <button ref={ref} onClick={handleChange} className="custom-button">{value}</button>
-    )
-  }
-  
-  export default Button
-  
+      ref.current.style.boxShadow = "4px 4px 0 0 #000";
+    }, 100);
+  };
+  return (
+    <button ref={ref} onClick={onClick} className="custom-button">
+      {value}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/TimerController.jsx
+++ src/components/TimerController.jsx
@@ -1,24 +1,55 @@
+import { useReducer } from "react";
+import Button from "./Button";
 
-import Button from "./Button"
+const reducer = (state, action) => {
+  if (action.type === "incremented_session" && state.sessionLength < 60) {
+    return { ...state, sessionLength: state.sessionLength + 1 };
+  }
+  if (action.type === "incremented_break" && state.breakLength < 60) {
+    return { ...state, breakLength: state.breakLength + 1 };
+  }
+  if (action.type === "decremented_session" && state.sessionLength > 1) {
+    return { ...state, sessionLength: state.sessionLength - 1 };
+  }
+  if (action.type === "decremented_break" && state.breakLength > 1) {
+    return { ...state, breakLength: state.breakLength - 1 };
+  } else return state;
+};
 
 const TimerController = () => {
-    return (
-      <div className="controller-wrapper">
-          <div className="session-wrapper">
-              <p>Session length</p>
-              <p>25min</p>
-              <Button value = "-"/>
-              <Button value = "+"/>
-          </div>
-          <div className="break-wrapper">
-              <p>Break length</p>
-              <p>5min</p>
-              <Button value = "-"/>
-              <Button value = "+"/>
-          </div>
+  const [state, dispatch] = useReducer(reducer, {
+    sessionLength: 25,
+    breakLength: 5,
+  });
+
+  return (
+    <div className="controller-wrapper">
+      <div className="session-wrapper">
+        <p>Session length</p>
+        <p>{state.sessionLength}min</p>
+        <Button
+          value="-"
+          onClick={() => dispatch({ type: "decremented_session" })}
+        />
+        <Button
+          onClick={() => dispatch({ type: "incremented_session" })}
+          value="+"
+        />
       </div>
-    )
-  }
-  
-  export default TimerController
-  
+      <div className="break-wrapper">
+        <p>Break length</p>
+        <p>{state.breakLength}min</p>
+        <Button
+          value="-"
+          onClick={() => dispatch({ type: "decremented_break" })}
+        />
+        <Button
+          value="+"
+          onClick={() => dispatch({ type: "incremented_break" })}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TimerController;
